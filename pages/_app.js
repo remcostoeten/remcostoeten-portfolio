df5f2e4627ed5c08f5a5d4e7c4a4eb26fd0d2955
remco stoeten <remcostoeten@hotmail.com>
2023-05-27 03:32:02 +0200

    Add top notice.

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import '../styles/styles.scss';
 import CursorPointer from 'components/Cursor';
 import ThemeSwitcher from 'components/header/ThemeSwitcher';
+import TopNotice from 'components/TopNotice';
 
 function app({ Component, pageProps }) {
 	const [mouseSize, setMouseSize] = useState(25);
 
 	return (
 		<>
-			<ThemeSwitcher />
+			<TopNotice />
 			<Component {...pageProps} />
 		</>
 	);
