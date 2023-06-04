c9f1e000197926a9d9f1a7718995459d17f06a0c
Remco Stoeten <remcostoeten@hotmail.com>
2023-06-05 00:36:51 +0200

    export const metadata = { 	title: 'Hello i am remco from ...🔥', 	description: 		'Remco Stoeten, front-end developer with six years experience aspiring to be more than just a so called "divjesschuiver. Site for contact information, showcase of projects (Works in progress) and several tools like URL Extract tools and todo / kanban board"', };

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,6 @@ const roboto = Roboto({
 	weight: ['300', '400', '500', '700', '900'],
 	style: ['italic', 'normal'],
 });
-export const metadata = {
-	title: 'Hello i am remco from ...🔥',
-	description:
-		'Remco Stoeten, front-end developer with six years experience aspiring to be more than just a so called "divjesschuiver. Site for contact information, showcase of projects (Works in progress) and several tools like URL Extract tools and todo / kanban board"',
-};
 
 export default function RootLayout({ children }) {
 	return (
