d476cab63eae603562f2dd3cbcabb4868444b855
remcostoeten <remcostoeten@hotmail.com>
2023-06-27 10:28:33 +0200

    WiP

--- app/page.jsx
+++ app/page.jsx
@@ -2,117 +2,117 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Intro from '@/components/hero/Intro';
-import Es6 from '@/components/icons/Es6';
-import Sass from '@/components/icons/Sass';
-import Photoshop from '@/components/icons/Photoshop';
-import HtmlIcon from '@/components/icons/Html';
-import ReactIcon from '@/components/icons/ReactIcon';
-import Magento from '@/components/icons/Magento';
-import BootstrapIcon from '@/components/icons/BootstrapIcon';
-import AdobeIcon from '@/components/icons/AdobeIcon';
-import StyledComponentIcon from '@/components/icons/StyledComponentIcon';
-import TypescriptIcon from '@/components/icons/TypescriptIcon';
-import JiraIcon from '@/components/icons/Jira';
-import Vim from '@/components/icons/Vim';
-import GitIcon from '@/components/icons/Git';
-import Sketch from '@/components/icons/Sketch';
-import Mui from '@/components/icons/Mui';
-import NpmIcon from '@/components/icons/NpmIcon';
-import Next from '@/components/icons/Next';
-import FirebaseLogo from '@/components/icons/FirebaseLogo';
-import Vue from '@/components/icons/Vue';
-import HeroContent from './../components/hero/HeroContent';
+// import Es6 from '@/components/icons/Es6';
+// import Sass from '@/components/icons/Sass';
+// import Photoshop from '@/components/icons/Photoshop';
+// import HtmlIcon from '@/components/icons/Html';
+// import ReactIcon from '@/components/icons/ReactIcon';
+// import Magento from '@/components/icons/Magento';
+// import BootstrapIcon from '@/components/icons/BootstrapIcon';
+// import AdobeIcon from '@/components/icons/AdobeIcon';
+// import StyledComponentIcon from '@/components/icons/StyledComponentIcon';
+// import TypescriptIcon from '@/components/icons/TypescriptIcon';
+// import JiraIcon from '@/components/icons/Jira';
+// import Vim from '@/components/icons/Vim';
+// import GitIcon from '@/components/icons/Git';
+// import Sketch from '@/components/icons/Sketch';
+// import Mui from '@/components/icons/Mui';
+// import NpmIcon from '@/components/icons/NpmIcon';
+// import Next from '@/components/icons/Next';
+// import FirebaseLogo from '@/components/icons/FirebaseLogo';
+// import Vue from '@/components/icons/Vue';
+// import HeroContent from './../components/hero/HeroContent';
 import LoginForm from '@/components/LoginForm';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { auth, googleAuthProvider } from '../lib/firebase';
-const IconList = [
-    AdobeIcon,
-    Es6,
-    HtmlIcon,
-    Sass,
-    Photoshop,
-    ReactIcon,
-    Magento,
-    Next,
-    BootstrapIcon,
-    StyledComponentIcon,
-    TypescriptIcon,
-    JiraIcon,
-    GitIcon,
-    Vim,
-    NpmIcon,
-    Sketch,
-    FirebaseLogo,
-    Mui,
-    Vue,
-];
+// const IconList = [
+//     AdobeIcon,
+//     Es6,
+//     HtmlIcon,
+//     Sass,
+//     Photoshop,
+//     ReactIcon,
+//     Magento,
+//     Next,
+//     BootstrapIcon,
+//     StyledComponentIcon,
+//     TypescriptIcon,
+//     JiraIcon,
+//     GitIcon,
+//     Vim,
+//     NpmIcon,
+//     Sketch,
+//     FirebaseLogo,
+//     Mui,
+//     Vue,
+// ];
 
-const IconNames = [
-    'AdobeIcon',
-    'ES6',
-    'HTML',
-    'Sass',
-    'Photoshop',
-    'React',
-    'Magento',
-    'Next.js',
-    'Styled Components',
-    'TypeScript',
-    'Jira',
-    'Git',
-    'Vim',
-    'Npm',
-    'Sketch',
-    'Firebase',
-    'Npm',
-    'Vue',
-    'Material-UI',
-];
+// const IconNames = [
+//     'AdobeIcon',
+//     'ES6',
+//     'HTML',
+//     'Sass',
+//     'Photoshop',
+//     'React',
+//     'Magento',
+//     'Next.js',
+//     'Styled Components',
+//     'TypeScript',
+//     'Jira',
+//     'Git',
+//     'Vim',
+//     'Npm',
+//     'Sketch',
+//     'Firebase',
+//     'Npm',
+//     'Vue',
+//     'Material-UI',
+// ];
 
-const IconUrls = [
-    'https://adobe.com',
-    'https://es6.io/',
-    'https://developer.mozilla.org/en-US/docs/Web/HTML',
-    'https://sass-lang.com/',
-    'https://www.adobe.com/products/photoshop.html',
-    'https://reactjs.org/',
-    'https://magento.com/',
-    'https://nextjs.org/',
-    'https://getbootstrap.com/',
-    'https://styled-components.com/',
-    'https://www.typescriptlang.org/',
-    'https://www.atlassian.com/software/jira',
-    'https://git-scm.com/',
-    'https://www.vim.org/',
-    'https://www.npmjs.com/',
-    'https://www.sketch.com/',
-    'https://firebase.google.com/',
-    'https://www.npmjs.com/',
-    'https://vuejs.org/',
-    'https://mui.com/',
-];
+// const IconUrls = [
+//     'https://adobe.com',
+//     'https://es6.io/',
+//     'https://developer.mozilla.org/en-US/docs/Web/HTML',
+//     'https://sass-lang.com/',
+//     'https://www.adobe.com/products/photoshop.html',
+//     'https://reactjs.org/',
+//     'https://magento.com/',
+//     'https://nextjs.org/',
+//     'https://getbootstrap.com/',
+//     'https://styled-components.com/',
+//     'https://www.typescriptlang.org/',
+//     'https://www.atlassian.com/software/jira',
+//     'https://git-scm.com/',
+//     'https://www.vim.org/',
+//     'https://www.npmjs.com/',
+//     'https://www.sketch.com/',
+//     'https://firebase.google.com/',
+//     'https://www.npmjs.com/',
+//     'https://vuejs.org/',
+//     'https://mui.com/',
+// ];
 
-const Icons = IconList.map((Icon, index) => ({
-    icon: <Icon />,
-    url: IconUrls[index],
-}));
+// const Icons = IconList.map((Icon, index) => ({
+//     icon: <Icon />,
+//     url: IconUrls[index],
+// }));
 
-const IconComponent = ({ icon, name, url }) => (
-    <div className="icons">
-        <div className="icons__icon flex flex-col-reverse items-center justify-center" data-aos="fade-up">
-            <Link href={url} target="_blank" rel="noopener noreferrer">
-                {icon}
-            </Link>
-            <span
-                style={{
-                    '--randomRotate': `${Math.floor(Math.random() * 7) - 3}deg`,
-                }}
-            >
-                {name}
-            </span>
-        </div>
-    </div>
-);
+// const IconComponent = ({ icon, name, url }) => (
+//     <div className="icons">
+//         <div className="icons__icon flex flex-col-reverse items-center justify-center" data-aos="fade-up">
+//             <Link href={url} target="_blank" rel="noopener noreferrer">
+//                 {icon}
+//             </Link>
+//             <span
+//                 style={{
+//                     '--randomRotate': `${Math.floor(Math.random() * 7) - 3}deg`,
+//                 }}
+//             >
+//                 {name}
+//             </span>
+//         </div>
+//     </div>
+// );
 
 export default function Home() {
     const [user, setUser] = useState(null);
@@ -147,7 +147,7 @@ export default function Home() {
                 <div className="w-1/2 h-screen">
                     <Intro />
                 </div>
-                <div className="w-1/2 h-screen overflow-auto">
+                {/* <div className="w-1/2 h-screen overflow-auto">
                     <div className="h-screen">
                         <HeroContent />
                     </div>
@@ -162,7 +162,7 @@ export default function Home() {
                             </>
                         )}
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     );
