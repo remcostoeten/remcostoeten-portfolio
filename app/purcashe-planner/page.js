9617740ebac739abe06b9553ac6ab85576fc1252
Remco Stoeten <remcostoeten@hotmail.com>
2023-07-10 20:49:00 +0200

    ppm

--- app/purcashe-planner/page.js
+++ app/purcashe-planner/page.js
@@ -6,6 +6,7 @@ import IconLogout from '@/components/icons/Logout';
 import ItemForm from '@/components/uitzet/ItemForm';
 import ItemTable from '@/components/uitzet/ItemTable';
 import { addItem, getItems, updateItem, deleteItem, storage } from '@/lib/firebase';
+import SignInClerk from './../../components/SignInClerk';
 const useClient = () => {
     const [user, setUser] = useState(null);
     const [items, setItems] = useState([]);
@@ -248,20 +249,14 @@ const Page = () => {
         <>
             <SignInClerk />
             <div className="w-max-[1600px] bg-white p-6 mx-auto px-4 sm:px-8">
-                {user ? (
-                    <div>
-                        <h1 className="text-2xl pl-1 border-bottom mt-8 font-semibold mb-4">Welkom, {user.displayName}</h1>
-                        <p className="mb-4">
-                            Create lists, for instance all the items i'll be needing to purcashe for when i'm moving on my own. Personal lists are unique per logged in user so no one can see the lists
-                            you've made.
-                        </p>
-                        <span onClick={handleLogout} className="absolute right-3 top-3">
-                            <IconLogout />
-                        </span>
-                    </div>
-                ) : (
-                    <LoginSection handleLogin={handleLogin} />
-                )}
+                <h1 className="text-2xl pl-1 border-bottom mt-8 font-semibold mb-4">Welkom, {user?.displayName}</h1>
+                <p className="mb-4">
+                    Create lists, for instance all the items i'll be needing to purcashe for when i'm moving on my own. Personal lists are unique per logged in user so no one can see the lists you've
+                    made.
+                </p>
+                <span onClick={handleLogout} className="absolute right-3 top-3">
+                    <IconLogout />
+                </span>
 
                 <ItemForm
                     title={title}
